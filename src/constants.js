326f0dffb6483f2428d64587f70c68580e50340a
Putri Karunia <putri@cotter.app>
2020-11-02 18:33:13 -0800

    Add cotter worker domain

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 // Enter your own Cotter api keys and Cloudflare subdomain here
-export const COTTER_API_KEY_ID = "<YOUR COTTER API KEY ID>";
-export const CLOUDFLARE_WORKER_DOMAIN = "YOURSUBDOMAIN.workers.dev";
+export const COTTER_API_KEY_ID = "229d172c-3674-4d0d-b3bf-43c429eb3ab4";
+export const CLOUDFLARE_WORKER_DOMAIN = "cottercalendar.workers.dev";
