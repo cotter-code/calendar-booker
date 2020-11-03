d6a04d9f4d135223f7fb69e6624395160335728f
Putri Karunia <putri@cotter.app>
2020-11-02 18:42:02 -0800

    Use cotter-react

--- .yalc/cotter-react/dist/components/Loading/index.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-import "./styles.scss";
-declare function Loading({ loading }: {
-    loading: boolean;
-}): JSX.Element;
-export default Loading;
--- .yalc/cotter-react/dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import CotterProvider from "./auth/userProvider";
-import CotterContext from "./auth/userContext";
-import withAuthenticationRequired from "./auth/withAuthenticationRequired";
-import LoginForm from "./components/LoginForm";
-export { CotterContext, CotterProvider, LoginForm, withAuthenticationRequired };
